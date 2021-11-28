6c5ddb84b4bfbbb053b26a9896fe8f3856bad077
B H An <anbyunghoon0107@gamil.com>
2021-11-28 23:37:21 +0900

    readme update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.get('/', ctx => {
+    ctx.body = '홈';
+});
+router.get('/about', ctx => {
+    ctx.body = '소개';
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log('Listening to port 4000');
