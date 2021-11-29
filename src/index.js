c6c365156f5b98f01e7b581fe1a377ac1e4ffc3f
안병훈 <82787408+dhshffnfn123@users.noreply.github.com>
2021-11-29 21:05:00 +0900

    라우터 파라미터 설정해보기

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ router.get('/', ctx => {
     ctx.body = '홈';
 });
 router.get('/about/:name?', ctx => {
-    ctx.body = '소개';
+    const { name } = ctx.params;
+    // name의 존재 유무에 따라 다른 결과 출력
+    ctx.body = name ? `${name}의 소개 ` : '소개';
+});
+
+router.get('/posts', ctx => {
+    const { id } = ctx.query;
+    // id의 존재 유무에 따라 다른 결과 출력
+    ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
 });
 
 // app 인스턴스에 라우터 적용
