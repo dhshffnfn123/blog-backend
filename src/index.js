06a0ac2bb1b709f7d88e99d77cb7502c6f0217c9
안병훈 <82787408+dhshffnfn123@users.noreply.github.com>
2021-11-30 15:54:28 +0900

    install koa-bodyParser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 
@@ -8,6 +9,9 @@ const router = new Router();
 
 // 라우터 설정
 router.use('/api', api.routes()); // api 라우트 적용
+
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
