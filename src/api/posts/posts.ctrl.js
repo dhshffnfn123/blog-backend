c555be71dabd16e9b044ac5156049aab278d9102
안병훈 <82787408+dhshffnfn123@users.noreply.github.com>
2021-11-30 19:32:47 +0900

    스키마 설정 중

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -14,7 +14,7 @@ const posts = [
  POST /api/posts
  { title, body }
 */
-exports.write = ctx => {
+export const write = ctx => {
     // REST API의 Request Body는 ctx.request.body에서 조회할 수 있다.
     const { title, body } = ctx.request.body;
     postId += 1; // postId 값에 1을 더함
@@ -27,7 +27,7 @@ exports.write = ctx => {
  포스트 목록 조회
  GET /api/posts
 */
-exports.list = ctx => {
+export const list = ctx => {
     ctx.body = posts;
 };
 
@@ -35,7 +35,7 @@ exports.list = ctx => {
 특정 포스트 조회
 GET /api/posts/:id 
 */
-exports.read = ctx => {
+export const read = ctx => {
     const { id } = ctx.params;
     // 주어진 id 값으로 포스트를 찾는다.
     // 파라미터로 받아온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나 비교할 p.id 값을 문자열로 변경해야 한다.
@@ -53,7 +53,7 @@ exports.read = ctx => {
 // 특정 포스트 제거
 // DELETE /api/posts/:id
 
-exports.remove = ctx => {
+export const remove = ctx => {
     const { id } = ctx.params;
     // 해당 id를 가진 post가 몇 번째인지 확인
     const index = posts.findIndex(p => p.id.toString() === id);
@@ -72,7 +72,7 @@ exports.remove = ctx => {
 
 // 특정 포스트 수정
 // PUT /api/posts/:id { title, body }
-exports.replace = ctx => {
+export const replace = ctx => {
     // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용한다.
     const { id } = ctx.params;
     // 해당 id를 가진 post가 몇번째 인지 확인
@@ -97,7 +97,7 @@ exports.replace = ctx => {
 // 포스트 수정 ( 특정 필드 변경)
 // PATCH /api/posts/:id { title, body }
 
-exports.update = ctx => {
+export const update = ctx => {
     // PATCH 메서드는 주어진 필드만 교체한다.
     const { id } = ctx.params;
     // 해당 id를 가진 post가 몇 번째인지 확인한다.
