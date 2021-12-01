14e0e7aab28231ecca0dbdaf845ee4f42aec1e62
B H An <anbyunghoon0107@gamil.com>
2021-12-01 23:10:54 +0900

    use Joi

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -5,8 +5,12 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router(); // /api/posts/:id
+post.get('/', postsCtrl.read);
+post.delete('/', postsCtrl.remove);
+post.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,7 +1,36 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400; // Bad Request
+        return;
+    }
+    return next();
+};
 
 // POST /api/posts { title : 제목, body : 내용, tags [태그1, 태그2]}
 export const write = async ctx => {
+    const schema = Joi.object().keys({
+        // 객체가 다음 필드를 가지고 있음을 검증
+        title: Joi.string().required(), // required()가 있으면 필수 항목
+        body: Joi.string().required(),
+        tags: Joi.array()
+            .items(Joi.string())
+            .required(), // 문자열로 이루어진 배열
+    });
+
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400; // Bad Request
+    }
+
+
     const { title, body, tags } = ctx.request.body;
     const post = new Post ({
         title,
