6f2cf6747d5f93ed595223725cd2256f324f728f
B H An <anbyunghoon0107@gamil.com>
2021-12-01 22:17:48 +0900

    api 작성 완료

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -16,10 +16,56 @@ export const write = async ctx => {
     }
 };
 
-export const list = ctx => {};
+// GET /api/posts
+export const list = async ctx => {
+    try {
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
 
-export const read = ctx => {};
+// GET /api/posts/:id
+export const read = async ctx => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findById(id).exec();    // 특정 id를 가진 데이터를 조회할 때는 findById() 함수를 사용한다.
+        if (!post) {
+            ctx.status = 404; // NOT FOUND
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
 
-export const remove = ctx => {};
+// DELETE /api/posts/:id
+export const remove = async ctx => {
+    const { id } = ctx.params;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
 
-export const update = ctx => {};
+// PATCH /api/posts/:id { title : 수정, body : 수정 내용, tags : [수정, 태그] }
+export const update = async ctx => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new : true, // 이 값을 설정하면 업데이트된 데이터를 반환한다.
+                        // false일 때는 업데이트되기 전의 데이터를 반환한다.
+        }).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
